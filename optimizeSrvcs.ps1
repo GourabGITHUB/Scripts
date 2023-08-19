d8c022c72a6a27ed8f9d23eae8b607eb59cd8b14
gourav chatterjee <chatterjeeg986@gmail.com>
2023-08-19 15:48:37 +0530

    replaced winupdate.ps1 with optimizesrvcs

---
+++ optimizeSrvcs.ps1
@@ -0,0 +1,8 @@
+$chkSrvcs = get-service wuauserv, vaultsvc, upnphost, ssdpsrv, policyagent, ikeext, fontcache, fdrespub, fdphost
+>> if ($chkSrvcs.Status -eq "Running"){
+>> echo "stopping services:"; stop-service fdphost, fdrespub, fontcache, ikeext, policyagent, ssdpsrv, upnphost, vaultsvc;
+>> get-service fdphost, fdrespub, fontcache, ikeext, policyagent, ssdpsrv, upnphost, vaultsvc
+>> }
+>> else {
+>> echo "services are not running"
+>> }
--- winupdate.ps1
+++
@@ -1,6 +0,0 @@
-$checkservice = get-service wuauserv
-if ($checkservice.Status -eq "Running"){
-stop-service wuauserv; set-service -StartupType Disabled wuauserv; get-service wuauserv}
-else {
-echo "service is not running"}
-
