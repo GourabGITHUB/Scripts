e67d8515f04b624c49055b09f3dfc11f7f6748aa
gourav chatterjee <chatterjeeg986@gmail.com>
2023-08-21 11:55:11 +0530

    updated script

--- optimizeSrvcs.ps1
+++ optimizeSrvcs.ps1
@@ -1,6 +1,6 @@
 $chkSrvcs = get-service wuauserv, vaultsvc, upnphost, ssdpsrv, policyagent, ikeext, fontcache, fdrespub, fdphost
 if ($chkSrvcs.Status -eq "Running"){
-echo "stopping services:"; stop-service fdphost, fdrespub, fontcache, ikeext, policyagent, ssdpsrv, upnphost, vaultsvc;
+echo "stopping services:"; stop-service -f fdphost, fdrespub, fontcache, ikeext, policyagent, ssdpsrv, upnphost, vaultsvc;
 get-service fdphost, fdrespub, fontcache, ikeext, policyagent, ssdpsrv, upnphost, vaultsvc
 }
 else {
